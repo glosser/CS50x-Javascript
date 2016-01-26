eba6b1d5062b7a969cbeb6e2377a67e3e3157258
glosser <benjiglos1@gmail.com>
2016-01-25 19:55:41 -0500

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,66 @@
+FORTUNE TELLER
+
+
+var children;
+
+var partner;
+
+var location;
+
+var occupation;
+
+var future = 'You will be a ' + occupation + ' in ' + location + 
+', and married to ' + partner + ' '+ ' with ' + children + 'children.';
+
+console.log(future);
+
+
+AGE CALCULATOR
+
+var curyear;
+
+var birthyear;
+
+var age = curyear - birthyear;
+
+console.log('They are either ' + age + ' or ' + (age -1));
+
+LIFETIME SUPPLY CALCULATOR
+
+var curage;
+
+var maxage;
+
+var dayamount;
+
+var total = (dayamount * 365) * (maxage - curage);
+
+var lifetime = 'You will need ' + total + ' to last you until the ripe old age of ' + maxage;
+
+console.log(lifetime);
+
+GEOMETRIZER
+
+var radius;
+
+var circumference = 2 * 3.14159 * radius;
+
+console.log('The circumference is ' + circumference);
+
+var area = 3.14159 * radius * radius;
+
+console.log('The area is ' + area);
+
+TEMPERATURE CONVERTER
+
+var celsius;
+
+var c2f = ((celsius  * 9) / 5) + 32);
+
+console.log(celsius + '°C is ' + c2f + '°F'); 
+
+var fahrenheit;
+
+var f2c = ((fahrenheit - 32) * 5) / 9);
+
+console.log(fahrenheit + '°F is ' + f2c + '°C');
