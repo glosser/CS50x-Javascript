fb0fa49b6142737217485d0c7ce2a1e606b21b0e
glosser <benjiglos1@gmail.com>
2016-02-02 17:03:27 -0500

    Create controlflow.js

---
+++ controlflow.js
@@ -0,0 +1,55 @@
+NUMBER
+function greaterNum (num1, num2) {
+console.log('The greater number of ' + num1 + 'and ' + num2 'is' math.Max(num1, num2));
+}
+greaterNum (5, 10);
+greaterNum (6, 2);
+greaterNum (100, 200);
+
+WORLD TRANSLATOR
+function helloWorld 
+if (lang == 'es')
+	return 'Hola, mundo';
+} else if (lang = 'fr')
+	return 'Bonjour, monde';
+} else
+	return 'Hello, World';
+}
+}
+
+
+GRADE ASSIGNER
+function assignGrade (number) {
+	if (90 <= number <=100)
+		alert("A");
+	else if (80<= number < 90)
+		alert("B");
+	else if (70 <= number < 80)
+		alert("C");
+	else if (60 <= number < 70)
+		alert("D");
+	else if (number < 60)
+		alert("F)");
+}
+assignGrade(99);
+assignGrade(65);
+assignGrade(73);
+
+PLURALIZER
+function pluralize (noun, num) {
+	if (num = 1)
+		alert ("1" + noun);
+	else if (1 < num)
+		alert (num + noun + "s");
+	else if (num = 0)
+		alert ("0" + noun + "s");
+}
+pluralize (dog, 5);
+pluralize (cat, 1);
+pluralize (swan, 0);
+
+EVEN/ODD REPORTER
+for (var counter = 0; counter < 21; counter = counter + 1)
+	if (counter / 2)
+	console.log(counter + 'is ' + 'even');
+	else console.log (counter 'is ' + 'odd');
