061924b4f903ba32d602ea77d32ae5c45855fcb5
glosser <benjiglos1@gmail.com>
2016-02-09 17:47:54 -0500

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,19 @@
+<!DOCTYPE js>
+var history = history.length;
+var r = confirm(history.length >= 1)
+if (r= true)
+{
+	txt = "Back";
+}
+
+var time = document.getElementById("myTime").innerHTML;
+setTimeout(function(){ alert(time); }, 1000);
+<embed rc="myTime">
+
+<ul>
+  <li>Go to work</li>
+  <li>Exercise</li>
+  <li>Watch TV</li>
+</ul>
+
+var todo = prompt("What do you like to do?")
